358cce2e57c9b1f43ffae4fe64df8ae1ca9dd416
KoriWalsh <korissarobertson@gmail.com>
2018-03-13 09:44:21 -0600

    Client-Server Architecture

---
+++ WebForms/Client-Server Demo/ServerSide/BLL/CodeDemoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataEntities;
+using ServerSide.DAL;
+
+namespace ServerSide.BLL
+{
+    public class CodeDemoController
+    {
+        public List<CodeDemo> ListAllDemos()
+        {
+            using (var context = new DemoLibraryContext())
+            {
+                return context.CodeDemos.ToList();
+            }
+        }
+    }
+}
---
+++ WebForms/Client-Server Demo/ServerSide/DAL/DemoLibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using DataEntities;
+using System.Data.Entity;
+
+
+namespace ServerSide.DAL
+{
+    internal class DemoLibraryContext: DbContext
+    {
+        public DemoLibraryContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<CodeDemo> CodeDemos { get; set; }
+
+    }
+}
